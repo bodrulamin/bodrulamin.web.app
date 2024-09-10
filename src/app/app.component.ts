3742b7435f1e190a9a58c97db0d51429fea2a292
bodrulamin <bodrulaminiu@gmail.com>
2024-09-10 17:36:42 +0600

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Button} from "primeng/button";
+import {ToolbarModule} from "primeng/toolbar";
+import {SplitButtonModule} from "primeng/splitbutton";
+import {InputTextModule} from "primeng/inputtext";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Button, ToolbarModule, SplitButtonModule, InputTextModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'bodrulamin.web.app';
+  title = 'bodrulamin';
 }
